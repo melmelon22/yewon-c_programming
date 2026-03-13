44ed4e0a8716290bd23c5907147110425f0bd4be
Han Yewon <melmelon@yonsei.ac.kr>
2026-03-13 05:05:32 +0000

    data type

---
+++ lect1/1st.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char strc[5] = "abcdeswhoqs";
+    printf("strc %%c: %c\n", strc);
+    printf("strc %%d: %d\n", strc);
+
+    printf("strc %%s: %s\n", strc);
+
+    return 0;
+}
