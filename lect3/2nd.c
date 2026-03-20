51bc69130bb88121c2135791f73ebf995464fb34
Han Yewon <melmelon@yonsei.ac.kr>
2026-03-20 04:21:59 +0000

    switch_case

---
+++ lect3/2nd.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int score = 85;
+
+    switch (score / 10) {
+    case 10:              //same as case 9
+    case 9: 
+        printf("Grade: A\n");
+        break;
+    case 8:
+        printf("Grade: B\n");
+        break;
+    case 7:
+        printf("Grade: C\n");
+        break;
+    case 6:
+        printf("Grade: D\n");
+        break;
+    default:
+        printf("Grade: F\n");
+        break;
+    }
+
+
+
+    return 0;
+}
